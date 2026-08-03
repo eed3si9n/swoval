60574b78a0da0918bc64853601b13aa64aaae94f
Eugene Yokota <eed3si9n@gmail.com>
2026-08-02 20:55:46 -0400

    Increase polling interval to 500ms

--- files/shared/src/test/scala/com/swoval/files/PathWatcherTest.scala
+++ files/shared/src/test/scala/com/swoval/files/PathWatcherTest.scala
@@ -500,7 +500,7 @@ object PollingPathWatcherTest extends PathWatcherTest {
   override def defaultWatcher(callback: PathWatchers.Event => _, followLinks: Boolean)(implicit
       testLogger: TestLogger
   ): PathWatcher[PathWatchers.Event] = {
-    val res = PathWatchers.polling(followLinks, 100, TimeUnit.MILLISECONDS)
+    val res = PathWatchers.polling(followLinks, 500, TimeUnit.MILLISECONDS)
     res.addObserver(callback)
     res
   }
